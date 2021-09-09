ab315e831b73586ab8027983b14a65840e01b2c3
PO8OP <zhangxinjunj@163.com>
2021-09-09 15:14:07 +0800

    init commit

---
+++ src/main/java/com/po8op/servicegateway/ServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.po8op.servicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/po8op/servicegateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.po8op.servicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
